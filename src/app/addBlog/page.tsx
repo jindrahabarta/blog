46a68095d70bf00c965f52c1e935f6dda7d28e77
jindrahabarta <jindriskuv@email.cz>
2024-08-18 10:05:34 +0200

    upravene use State nazvy

--- src/app/addBlog/page.tsx
+++ src/app/addBlog/page.tsx
@@ -1,114 +1,122 @@
-"use client";
-import React, { useState } from "react";
-import useDb from "../_utils/useDb";
+'use client'
+import React, { useState } from 'react'
+import useDb from '../_utils/useDb'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
 
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+const AddBlog = () => {
+    const db = useDb()
+    const [BlogTitle, setBlogTitle] = useState<string>('')
+    const [BlogText, setBlogText] = useState<string>('')
+    const [BlogAuthor, setBlogAuthor] = useState<string>('')
+    const [Show, setShow] = useState(false)
 
-const addBlog = () => {
-  const db = useDb();
-  const [blogTitle, setBlogTitle] = useState<string>("");
-  const [blogText, setBlogText] = useState<string>("");
-  const [blogAuthor, setBlogAuthor] = useState<string>("");
-  const [show, setShow] = useState(false);
+    const Send = (e: any) => {
+        e.preventDefault()
 
-  const Send = (e: any) => {
-    e.preventDefault();
+        addDoc(collection(db, 'blogs'), {
+            createdAt: serverTimestamp(),
+            title: BlogTitle,
+            text: BlogText,
+            author: BlogAuthor,
+        })
+        setShow(true)
+        setTimeout(() => {
+            setShow(false)
+        }, 2000)
 
-    addDoc(collection(db, "blogs"), {
-      createdAt: serverTimestamp(),
-      title: blogTitle,
-      text: blogText,
-      author: blogAuthor,
-    });
-    setShow(true);
-    setTimeout(() => {
-      setShow(false);
-    }, 2000);
+        console.log('added')
+        setBlogTitle('')
+        setBlogAuthor('')
+        setBlogText('')
+    }
 
-    console.log("added");
-    setBlogTitle("");
-    setBlogAuthor("");
-    setBlogText("");
-  };
+    return (
+        <main className="flex flex-col gap-2 items-center pt-10">
+            <h1 className="font-bold">Add blog form</h1>
+            <div className="border-gray-200 border shadow-sm shadow-gray-200 bg-white w-96">
+                <div className="w-full h-4 bg-red-200"></div>
 
-  return (
-    <main className="flex flex-col gap-2 items-center pt-10">
-      <h1 className="font-bold">Add blog form</h1>
-      <div className="border-gray-200 border shadow-sm shadow-gray-200 bg-white w-96">
-        <div className="w-full h-4 bg-red-200"></div>
+                <form className=" p-4" onSubmit={Send}>
+                    <table className="w-full">
+                        <tbody className="flex flex-col gap-2">
+                            <tr>
+                                <td className="w-fit pr-2">
+                                    <label htmlFor="title">Title:</label>
+                                </td>
+                                <td className="w-full">
+                                    <input
+                                        onChange={(e) =>
+                                            setBlogTitle(e.target.value)
+                                        }
+                                        className="outline-none border-b w-1/2 border-black focus:w-full focus:border-red-200 duration-200"
+                                        max={25}
+                                        min={3}
+                                        value={BlogTitle}
+                                        required
+                                        type="text"
+                                        id="title"
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="w-fit pr-2">
+                                    <label htmlFor="author">Author:</label>
+                                </td>
+                                <td className="w-full">
+                                    <input
+                                        onChange={(e) =>
+                                            setBlogAuthor(e.target.value)
+                                        }
+                                        className="outline-none border-b w-1/2 border-black focus:w-full focus:border-red-200 duration-200"
+                                        max={10}
+                                        min={3}
+                                        value={BlogAuthor}
+                                        required
+                                        type="text"
+                                        id="author"
+                                    />
+                                </td>
+                            </tr>
 
-        <form className=" p-4" onSubmit={Send}>
-          <table className="w-full">
-            <tbody className="flex flex-col gap-2">
-              <tr>
-                <td className="w-fit pr-2">
-                  <label htmlFor="title">Title:</label>
-                </td>
-                <td className="w-full">
-                  <input
-                    onChange={(e) => setBlogTitle(e.target.value)}
-                    className="outline-none border-b w-1/2 border-black focus:w-full focus:border-red-200 duration-200"
-                    max={25}
-                    min={3}
-                    value={blogTitle}
-                    required
-                    type="text"
-                    id="title"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="w-fit pr-2">
-                  <label htmlFor="author">Author:</label>
-                </td>
-                <td className="w-full">
-                  <input
-                    onChange={(e) => setBlogAuthor(e.target.value)}
-                    className="outline-none border-b w-1/2 border-black focus:w-full focus:border-red-200 duration-200"
-                    max={10}
-                    min={3}
-                    value={blogAuthor}
-                    required
-                    type="text"
-                    id="author"
-                  />
-                </td>
-              </tr>
+                            <tr>
+                                <td className="w-fit pr-2 h-full align-top">
+                                    <label
+                                        className=" self-start"
+                                        htmlFor="text"
+                                    >
+                                        Text:
+                                    </label>
+                                </td>
+                                <td className="w-full">
+                                    <textarea
+                                        onChange={(e) =>
+                                            setBlogText(e.target.value)
+                                        }
+                                        id="text"
+                                        rows={8}
+                                        value={BlogText}
+                                        required
+                                        className="outline-none border-b border-black w-full focus:border-red-200 duration-200"
+                                    ></textarea>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td className="flex justify-end">
+                                    <input
+                                        className=" border border-gray-200 shadow-md shadow-gray-200 px-2 hover:shadow-inner duration-200 hover:cursor-pointer"
+                                        type="submit"
+                                        value="Odeslat"
+                                    />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    {Show && <p className="w-full   text-end">Sent</p>}
+                </form>
+            </div>
+        </main>
+    )
+}
 
-              <tr>
-                <td className="w-fit pr-2 h-full align-top">
-                  <label className=" self-start" htmlFor="text">
-                    Text:
-                  </label>
-                </td>
-                <td className="w-full">
-                  <textarea
-                    onChange={(e) => setBlogText(e.target.value)}
-                    id="text"
-                    rows={8}
-                    value={blogText}
-                    required
-                    className="outline-none border-b border-black w-full focus:border-red-200 duration-200"
-                  ></textarea>
-                </td>
-              </tr>
-              <tr>
-                <td></td>
-                <td className="flex justify-end">
-                  <input
-                    className=" border border-gray-200 shadow-md shadow-gray-200 px-2 hover:shadow-inner duration-200 hover:cursor-pointer"
-                    type="submit"
-                    value="Odeslat"
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          {show && <p className="w-full   text-end">Sent</p>}
-        </form>
-      </div>
-    </main>
-  );
-};
-
-export default addBlog;
+export default AddBlog
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,59 +1,62 @@
-"use client";
-
-import useFetchCollection from "../_utils/useFetchCollection";
+'use client'
+import useFetchCollection from '../_utils/useFetchCollection'
 
 import {
-  CollectionReference,
-  DocumentData,
-  collection,
-  deleteDoc,
-  doc,
-} from "firebase/firestore";
-import useDb from "../_utils/useDb";
+    CollectionReference,
+    DocumentData,
+    deleteDoc,
+    doc,
+} from 'firebase/firestore'
+import useDb from '../_utils/useDb'
 
 const Admin = () => {
-  const db = useDb();
-  const { data: blogs, loaded, error } = useFetchCollection("blogs");
+    const db = useDb()
+    const { data: blogs, loaded, error } = useFetchCollection('blogs')
 
-  const handleDelete = async (id: any) => {
-    await deleteDoc(doc(db, "blogs", id));
-  };
+    const handleDelete = async (id: any) => {
+        await deleteDoc(doc(db, 'blogs', id))
+    }
 
-  return (
-    <main className="flex flex-col gap-2 items-center pt-10">
-      <h1 className="font-bold">Admin page</h1>
+    return (
+        <main className="flex flex-col gap-2 items-center pt-10">
+            <h1 className="font-bold">Admin page</h1>
 
-      {loaded ? (
-        blogs.length > 0 ? (
-          <div className="border-gray-200 shadow-sm shadow-gray-200 bg-white flex flex-col p-4 gap-2 w-96">
-            {blogs.map((blog: any) => (
-              <div className="flex justify-between items-center" key={blog.id}>
-                <div>
-                  <h1>{blog.title}</h1>
-                  <p className=" text-gray-500 text-xs">{blog.author}</p>
-                </div>
+            {loaded ? (
+                blogs.length > 0 ? (
+                    <div className="border-gray-200 shadow-sm shadow-gray-200 bg-white flex flex-col p-4 gap-2 w-96">
+                        {blogs.map((blog: any) => (
+                            <div
+                                className="flex justify-between items-center"
+                                key={blog.id}
+                            >
+                                <div>
+                                    <h1>{blog.title}</h1>
+                                    <p className=" text-gray-500 text-xs">
+                                        {blog.author}
+                                    </p>
+                                </div>
 
-                <div
-                  onClick={() => handleDelete(blog.id)}
-                  className="group h-4 w-4 flex flex-col justify-center hover:gap-0 gap-0.5 items-center rounded-lg bg-red-300 hover:bg-red-500 shadow-sm shadow-gray-400 hover:shadow-inner hover:cursor-pointer duration-200"
-                >
-                  <div className=" group-hover:rotate-45 w-3 h-0.5 rounded-md bg-black duration-200"></div>
-                  <div className="group-hover:-rotate-45 w-3 h-0.5 rounded-md bg-black duration-500"></div>
-                </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p>There is no blog</p>
-        )
-      ) : (
-        <p>Loading...</p>
-      )}
-    </main>
-  );
-};
+                                <div
+                                    onClick={() => handleDelete(blog.id)}
+                                    className="group h-4 w-4 flex flex-col justify-center hover:gap-0 gap-0.5 items-center rounded-lg bg-red-300 hover:bg-red-500 shadow-sm shadow-gray-400 hover:shadow-inner hover:cursor-pointer duration-200"
+                                >
+                                    <div className=" group-hover:rotate-45 w-3 h-0.5 rounded-md bg-black duration-200"></div>
+                                    <div className="group-hover:-rotate-45 w-3 h-0.5 rounded-md bg-black duration-500"></div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <p>There is no blog</p>
+                )
+            ) : (
+                <p>Loading...</p>
+            )}
+        </main>
+    )
+}
 
-export default Admin;
+export default Admin
 function deletteDoc(arg0: CollectionReference<DocumentData, DocumentData>) {
-  throw new Error("Function not implemented.");
+    throw new Error('Function not implemented.')
 }
